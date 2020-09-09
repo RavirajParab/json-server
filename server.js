b797a45983c0e3bec22871dfd2c5a84cf30f2ffd
Raviraj Parab <ravirajparab44@gmail.com>
2020-09-09 18:25:43 +0530

    init logic

--- server.js
+++ server.js
@@ -2,15 +2,6 @@ const jsonServer = require('json-server');
 const cors = require("cors");
 const jsonResetMiddleware = require("json-server-reset");
 
-const server = jsonServer.create();
-server.use(resetDB);
-const router = jsonServer.router('db.json');
-
-
-const middlewares = jsonServer.defaults();
-const FinanceApp = require('./Routes/Finance');
-const port = process.env.PORT || 3000;
-
 const resetDB = (req, res, next) => {
     if (req.method === 'GET' && req.url.endsWith('/reset/')) {
       const db = JSON.parse(fs.readFileSync('db.json', 'utf8'));
@@ -20,6 +11,17 @@ const resetDB = (req, res, next) => {
       next();
     }
   };
+
+const server = jsonServer.create();
+server.use(resetDB);
+const router = jsonServer.router('db.json');
+
+
+const middlewares = jsonServer.defaults();
+const FinanceApp = require('./Routes/Finance');
+const port = process.env.PORT || 3000;
+
+
 
 server.use(cors());
 server.use('/finance',FinanceApp);
