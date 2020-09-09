cef037b9dbac86d52abb0a3657307b2f4b984cc0
Raviraj Parab <ravirajparab44@gmail.com>
2020-09-09 18:03:16 +0530

    init logic

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const jsonServer = require('json-server');
 const cors = require("cors");
+const jsonResetMiddleware = require("json-server-reset");
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+
 const middlewares = jsonServer.defaults();
 const FinanceApp = require('./Routes/Finance');
-const CleanApp = require('./Cleanup/cleanup');
 const port = process.env.PORT || 3000;
 
 server.use(cors());
 server.use('/finance',FinanceApp);
-server.use('/clean',CleanApp);
 server.use(middlewares);
+server.use(jsonResetMiddleware);
 server.use(router);
 server.listen(port);
